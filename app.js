745e6981495c4cebd650624629ddcdb9926cf107
Luis Enrique <luisjrcrash@gmail.com>
2021-06-08 02:24:42 -0500

    Examen

---
+++ app.js
@@ -0,0 +1,96 @@
+/*
+*       Escribir debajo de cada enunciado su solución.
+*       Comenta el código que consideres necesario. 
+*/
+
+/**
+ *  1. Create a new array with only red colored ice cream.
+
+const iceCreams = [
+    { flavor: 'pineapple', color: 'white' },
+    { flavor: 'strawberry', color: 'red' },
+    { flavor: 'watermelon', color: 'red' },
+    { flavor: 'kiwi', color: 'green' },
+    { flavor: 'mango', color: 'yellow' },
+    { flavor: 'pear', color: 'green' }
+];
+
+ */
+
+
+
+/**
+ 2. Create a new array of strings, with all the flavors of ice cream. 
+    const iceCreams = [
+    { flavor: 'pineapple', color: 'white' },
+    { flavor: 'strawberry', color: 'red' },
+    { flavor: 'watermelon', color: 'red' },
+    { flavor: 'kiwi', color: 'green' },
+    { flavor: 'mango', color: 'yellow' },
+    { flavor: 'pear', color: 'green' }
+    ];
+ */
+
+
+
+
+
+/**
+ * 3.  We want two arrays now: one for rebel pilots, the other one for imperials    
+
+let pilots = [
+    {
+        id: 2,
+        name: "Wedge Antilles",
+        faction: "Rebels",
+    },
+    {
+        id: 8,
+        name: "Ciena Ree",
+        faction: "Empire",
+    },
+    {
+        id: 40,
+        name: "Iden Versio",
+        faction: "Empire",
+    },
+    {
+        id: 66,
+        name: "Thane Kyrell",
+        faction: "Rebels",
+    }
+];  
+ */
+
+
+/*
+3. Consider a scenario in which we have multiple records of students and each student record has a name, ID, and marks attribute.
+
+Problem Statement: We are interested in retrieving only the name of the students and all the names should be in caps.
+Expected Result: ['JOHN', 'LIGA', 'YAGA', 'WICK']
+
+let studentRecords = [{ name: 'John', id: 123, marks: 98 },
+{ name: 'Liga', id: 101, marks: 23 },
+{ name: 'yaga', id: 200, marks: 45 },
+{ name: 'Wick', id: 115, marks: 75 }]
+
+*/
+
+
+
+/*
+4. Problem Statement: Suppose we have the same dataset as above but this time we want to get the details of students who scored more than 50 marks.
+*/
+
+/**
+ 5. Problem Statement: Retrieve the name of students who scored more than 50 marks and have id greater than 120. 
+ */
+
+
+
+
+
+
+
+
+
